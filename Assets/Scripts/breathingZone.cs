527896745add8857b5de2f8e44b4ef3d78ffb1cc
unknown <xjerab28@stud.fit.vutbr.cz>
2024-07-05 11:18:37 +0100

    feat: heart rate simulator

--- Assets/Scripts/breathingZone.cs
+++ Assets/Scripts/breathingZone.cs
@@ -3,36 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Haptics;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class breathingZone : MonoBehaviour
 {
     public bool isBreathing = false;
+    public bool isVibrating = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    void OnTriggerEnter(Collider other)
+    IEnumerator OnTriggerEnter(Collider other)
     {
         Debug.Log("COLLIDING");
         // if the controller is the left hand
         if (other.CompareTag("GameController") && isBreathing)
         {
             Debug.Log("VIBRATING");
-            var xrController = other.GetComponent<XRBaseController>();
+            isVibrating = true;
+            var xrController = other.GetComponent<HapticImpulsePlayer>();
             if (xrController != null)
             {
-                xrController.SendHapticImpulse(0.5f, 0.5f);
+                while (isVibrating)
+                {
+
+                    xrController.SendHapticImpulse(0.6f, 0.3f);
+
+                    yield return new WaitForSeconds(0.8f);
+                }
             }
             else
             {
                 Debug.Log("ERROR: WRONG CONTROLLER");
             }
+
         }
 
 
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("GameController"))
+        {
+            isVibrating = false;
+        }
     }
 }
